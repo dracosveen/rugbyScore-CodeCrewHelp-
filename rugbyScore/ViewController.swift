67f5f35c4314a387825c4ed07a526259e378e627
Richard Cumming <richard.cumming@outlook.com>
2019-11-17 11:48:06 +1100

    Initial Commit

---
+++ rugbyScore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rugbyScore
+//
+//  Created by Richard Cumming on 17/11/2019.
+//  Copyright © 2019 dracosveen's Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
