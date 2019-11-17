636e8a1121f7141f703c927ae5f7af0589d3117d
Richard Cumming <richard.cumming@outlook.com>
2019-11-17 12:02:50 +1100

    testing

--- rugbyScore/ViewController.swift
+++ rugbyScore/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var lblTries: UILabel!
+    @IBOutlet var lblConversions: UILabel!
+    @IBOutlet var lblTotalScore: UILabel!
+    
+    var tries: Int = 0
+    var conversions: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // init
+        lblTries.text = "\(0)"
+        lblConversions.text = "\(0)"
+        lblTotalScore.text = "\(0)"
+        
     }
-
-
+    
+    
+    @IBAction func cmdTry(_ sender: Any) {
+        tries+=5
+        lblTries.text = "Tries: \(tries)"
+        lblTotalScore.text = "Total Score: \(tries + conversions)"
+    }
+    
+    @IBAction func Conversion(_ sender: Any) {
+        conversions+=2
+        lblConversions.text = "Conversions: \(conversions)"
+        lblTotalScore.text = "Total Score: \(tries + conversions)"
+    }
+    
+    @IBAction func cmdPenalty(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func cmdDropgoal(_ sender: Any) {
+        
+    }
+    
 }
 
